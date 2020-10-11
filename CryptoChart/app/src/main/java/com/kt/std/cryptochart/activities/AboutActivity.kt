4baa95740f06c451a454e75761155fca7facdfd1
mpolyakov <polyakov4@yandex.ru>
2020-10-11 10:09:58 +0300

    Создание проекта, добавление информационного активити

---
+++ CryptoChart/app/src/main/java/com/kt/std/cryptochart/activities/AboutActivity.kt
@@ -0,0 +1,27 @@
+package com.kt.std.cryptochart.activities
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.kt.std.cryptochart.R
+import kotlinx.android.synthetic.main.activity_about.*
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        buttonRateApp.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=${applicationContext.packageName}")
+                )
+            )
+        }
+    }
+}
---
+++ CryptoChart/app/src/main/java/com/kt/std/cryptochart/activities/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.kt.std.cryptochart.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.kt.std.cryptochart.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId){
+            R.id.action_about -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+}
